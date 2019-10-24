aee79730dff2cec4d87c2f8b0edfa53eb2a2b0a8
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-24 12:21:38 +0100

    added pattern creational prototype

--- src/main/java/com/pattern/creational/prototype/MainPrototype.java
+++ src/main/java/com/pattern/creational/prototype/MainPrototype.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class MainPrototype {
 
-    static Employee employees = new Employee();
-
     public static void main(String[] args) throws CloneNotSupportedException {
+        Employee employees = new Employee();
         String[] listEmployees = {"John Snow", "Mark Snowman", "John Button", "Lisa Simpson"};
         employees.loadData(listEmployees);
 
         //Gets the Employee object using the clone method
         Employee employeesNew = (Employee) employees.clone();
         Employee employeesNew1 = (Employee) employees.clone();
-        System.out.println("emps List: " + employees.getEmpList());
 
         ArrayList<String> list0 = employeesNew.getEmpList();
         list0.add("Sarah Green");
-        System.out.println("empsNew List: " + list0);
 
         ArrayList<String> list1 = employeesNew1.getEmpList();
         list1.remove("Mark Snowman");
-        System.out.println("empsNew1 List: " + list1);
+
+        System.out.println("employees List: " + employees.getEmpList());
+        System.out.println("employeesNew List: " + list0);
+        System.out.println("employeesNew1 List: " + list1);
     }
 }
