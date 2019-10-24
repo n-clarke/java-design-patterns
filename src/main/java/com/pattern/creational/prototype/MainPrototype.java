6fb3799a7144e85f1da135d2e9b3cfeec12c24a0
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-24 12:31:34 +0100

    added pattern creational prototype

--- src/main/java/com/pattern/creational/prototype/MainPrototype.java
+++ src/main/java/com/pattern/creational/prototype/MainPrototype.java
@@ -1,26 +1,24 @@
 package com.pattern.creational.prototype;
 
-import java.util.ArrayList;
-
 public class MainPrototype {
 
     public static void main(String[] args) throws CloneNotSupportedException {
+        //Create an Employee object and adding employee data.
         Employee employees = new Employee();
         String[] listEmployees = {"John Snow", "Mark Snowman", "John Button", "Lisa Simpson"};
         employees.loadData(listEmployees);
 
-        //Gets the Employee object using the clone method
+        //Gets the Employee object using the clone method.
         Employee employeesNew = (Employee) employees.clone();
         Employee employeesNew1 = (Employee) employees.clone();
 
-        ArrayList<String> list0 = employeesNew.getEmpList();
-        list0.add("Sarah Green");
+        //Making changes to the cloned objects.
+        employeesNew.getEmpList().add("Sarah Green");
+        employeesNew1.getEmpList().remove("Mark Snowman");
 
-        ArrayList<String> list1 = employeesNew1.getEmpList();
-        list1.remove("Mark Snowman");
-
+        //Displaying Results.
         System.out.println("employees List: " + employees.getEmpList());
-        System.out.println("employeesNew List: " + list0);
-        System.out.println("employeesNew1 List: " + list1);
+        System.out.println("employeesNew List: " + employeesNew.getEmpList());
+        System.out.println("employeesNew1 List: " + employeesNew1.getEmpList());
     }
 }
