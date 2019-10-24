9b7f213b07bd8ee2fccff88fb230ab5ff683bd4b
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-24 15:05:44 +0100

    added pattern structural adapter

--- src/main/java/com/pattern/structural/adapter/Volt.java
+++ src/main/java/com/pattern/structural/adapter/Volt.java
@@ -3,8 +3,9 @@ package com.pattern.structural.adapter;
 public class Volt {
 
     private int volts;
-    public int getVolts() { return volts; }
-    public void setVolts(int volts) { this.volts = volts; }
+    public int getVolts() {
+        return volts;
+    }
 
     public Volt(int volts){
         this.volts = volts;
