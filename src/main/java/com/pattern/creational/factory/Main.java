cf5bc168546411f64f05f261e643f34a0c0acf5a
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-24 11:07:26 +0100

    added pattern creational factory

---
+++ src/main/java/com/pattern/creational/factory/Main.java
@@ -0,0 +1,10 @@
+package com.pattern.creational.factory;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer pc = Factory.getComputer("Apple MacBook Pro","8 GB","1 TB","2.4 GHz");
+        Computer server = Factory.getComputer("Server","32 GB","3 TB","5 GHz");
+        System.out.println("Factory PC Config::"+pc);
+        System.out.println("Factory Server Config::"+server);
+    }
+}
---
+++ src/main/java/com/pattern/creational/factory/Server.java
@@ -0,0 +1,26 @@
+package com.pattern.creational.factory;
+
+public class Server extends Computer {
+
+    private String ram, hdd, cpu;
+
+    public Server(String ram, String hdd, String cpu){
+        this.ram=ram;
+        this.hdd=hdd;
+        this.cpu=cpu;
+    }
+    @Override
+    public String getRAM() {
+        return this.ram;
+    }
+
+    @Override
+    public String getHDD() {
+        return this.hdd;
+    }
+
+    @Override
+    public String getCPU() {
+        return this.cpu;
+    }
+}
