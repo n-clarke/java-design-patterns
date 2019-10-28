4a72511e938e96b9ac3b329331ae63a60516e835
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-28 13:56:30 +0000

    added pattern behavioural template method.

--- src/main/java/com/pattern/behavioral/templatemethod/OnlinePurchase.java
+++ src/main/java/com/pattern/behavioral/templatemethod/OnlinePurchase.java
@@ -3,17 +3,17 @@ package com.pattern.behavioral.templatemethod;
 class OnlinePurchase extends TransactionProcessTemplate
 {
     @Override
-    public void productSelected() {
+    public final void productSelected() {
         System.out.println("Product added to online shopping cart.");
     }
 
     @Override
-    public void paymentTransaction() {
+    public final void paymentTransaction() {
         System.out.println("Online Payment through Visa Debt, Credit Card or PayPal.");
     }
 
     @Override
-    public void productDelivery() {
+    public final void productDelivery() {
         System.out.println("Product is shipped through post to customers delivery address.");
     }
 }
--- src/main/java/com/pattern/behavioral/templatemethod/StorePurchase.java
+++ src/main/java/com/pattern/behavioral/templatemethod/StorePurchase.java
@@ -2,17 +2,17 @@ package com.pattern.behavioral.templatemethod;
 
 class StorePurchase extends TransactionProcessTemplate {
     @Override
-    public void productSelected() {
+    public final void productSelected() {
         System.out.println("Customer has taken an item from a shelf.");
     }
 
     @Override
-    public void paymentTransaction() {
+    public final void paymentTransaction() {
         System.out.println("Customer has paid at counter using either Cash, POS or Gift Card.");
     }
 
     @Override
-    public void productDelivery() {
+    public final void productDelivery() {
         System.out.println("Product has been collected at store counter.");
     }
 }
