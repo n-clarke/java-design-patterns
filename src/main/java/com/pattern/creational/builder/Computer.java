5bf44f700979842079d0c743350f5583eddc2017
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-10-28 13:58:05 +0000

    Updated Design Patterns

--- src/main/java/com/pattern/creational/builder/Computer.java
+++ src/main/java/com/pattern/creational/builder/Computer.java
@@ -24,7 +24,7 @@ public class Computer {
     }
 
     //Computer Builder Class
-    public static class ComputerBuilder{
+    public static class ComputerBuilder {
 
         private String hdd, ram;
         private boolean isGraphicsCardEnabled, isBluetoothEnabled;
